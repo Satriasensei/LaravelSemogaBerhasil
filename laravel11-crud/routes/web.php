7347d0f0dc701a9e85972d026597b7fa09fdd77d
Satria <Adinatasatria8@gmailcom>
2025-07-30 13:00:06 +0800

    Membuat Controller

--- laravel11-crud/routes/web.php
+++ laravel11-crud/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ProductController;
+
+Route::get('/', fn() => redirect('/products'));
+Route::resource('products', ProductController::class);
+
